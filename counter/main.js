543329a1932d196f41ab9f36208bf4c74f03fb2b
Ankit Gupta <gankit389@gmail.com>
2020-09-13 19:45:26 -0400

    finished fourth project

---
+++ counter/main.js
@@ -0,0 +1,11 @@
+var counter = 0;
+
+function add(){
+    counter ++;
+    document.getElementById("count").innerHTML = counter;
+
+}
+function subtract(){
+    counter --;
+    document.getElementById("count").innerHTML = counter;
+}
